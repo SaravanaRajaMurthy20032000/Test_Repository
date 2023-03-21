d55609c0e05d369b1c2ca4608fe8927669f4cb0e
saravanaraja <saravanarajam20032000@gmail.com>
2023-03-21 09:48:00 +0530

    list_tuples

---
+++ practice_1.py
@@ -0,0 +1,16 @@
+# 1
+# list = [('mango',12),('orange',20),('apple',34),('peach',55)]
+# print(list)
+# 
+lst1 = input("enter the string: ")
+lst2 = input("enter the num: ")
+lst_tuple = list(zip(lst1,lst2))
+print(lst_tuple)
+# 
+list = [('saravana'),('indhu'),('teja'),('harish')]
+l = []
+for i in list:
+    if i[0]:
+        l.append(i)
+print(l)
+
