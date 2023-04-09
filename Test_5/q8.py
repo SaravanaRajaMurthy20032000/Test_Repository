013d75e258e94cfd3cd43875283043e5018f2333
Saravana-Erode <saravanaraja@istardevelopers.in>
2023-04-09 18:15:21 +0530

    tests

--- Test_5/q8.py
+++ Test_5/q8.py
@@ -1,5 +1,11 @@
 # Write a function ‘select_second’ with one argument 'L' which is a list to return the second element of the given list. 
 # If the list has no second element, it should return None.
 
-# def select_second(L):
-
+def select_second(L):
+    if len(L):
+        return L[1]
+    else:
+        return None
+    
+print(select_second([1,2,3,4,]))
+print(select_second(['a','s','d','f','g']))
